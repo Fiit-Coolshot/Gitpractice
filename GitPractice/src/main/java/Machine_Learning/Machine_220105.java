65d6831419ddef035a6a53ca136c462ab9f5af66
USER <USER@211.208.59.201>
2022-01-05 23:20:17 +0900

    project대비 machine leaning study

---
+++ GitPractice/src/main/java/Machine_Learning/Machine_220105.java
@@ -0,0 +1,10 @@
+package Machine_Learning;
+
+public class Machine_220105 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//project´ëºñ machine leaning study
+	}
+
+}
